92f3880e46f382b00ba1507ea44e06d9fbc6f0e8
alehuo <aleksi.huotala@helsinki.fi>
2020-04-20 08:55:39 +0300

    Update copyright comment block

--- src/utils.tsx
+++ src/utils.tsx
@@ -1,14 +1,13 @@
-/*!
+import React from "react";
+import { TextColor } from "./reducers/uiReducer";
+
+/**
  * Get the contrasting color for any hex color
  * (c) 2019 Chris Ferdinandi, MIT License, https://gomakethings.com
  * Derived from work by Brian Suda, https://24ways.org/2010/calculating-color-contrast/
  * @param  {String} A hexcolor value
  * @return {String} The contrasting color (black or white)
  */
-
-import React from "react";
-import { TextColor } from "./reducers/uiReducer";
-
 export const getContrast = (r: number, g: number, b: number): TextColor => {
   // Get YIQ ratio
   const yiq = (r * 299 + g * 587 + b * 114) / 1000;
